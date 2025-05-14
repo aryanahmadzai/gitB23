f2c203cd36963fb9c1d0ee949eb993d383c2a767
aryanahmadzai <111163486+aryanahmadzai@users.noreply.github.com>
2025-05-14 19:25:24 -0400

    1st commit

---
+++ src/main/java/aryan.java
@@ -0,0 +1,7 @@
+public class aryan {
+
+    public static void main(String[] args) {
+
+        String adskjfal;
+    }
+}
---
+++ src/main/java/nehal.java
@@ -0,0 +1,7 @@
+public class nehal {
+
+    public static void main(String[] args) {
+
+        int age=10;
+    }
+}
